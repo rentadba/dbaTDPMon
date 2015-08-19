586d889cf5896d399f7ae0c91a087809636f47df
Dan Andrei STEFAN <danandrei.stefan@gmail.com>
2015-08-19 10:44:01 +0300

    create simple batches in order to automate maintenance plan when running as agent on SQL Express edition

---
+++ source/maintenance-plan/job-scripts/sql-express-simple-batches/run-database-optimization-maintenance.sql
@@ -0,0 +1,46 @@
+-- ============================================================================
+-- Copyright (c) 2004-2015 Dan Andrei STEFAN (danandrei.stefan@gmail.com)
+-- ============================================================================
+-- Author			 : Dan Andrei STEFAN
+-- Create date		 : 19.08.2015
+-- Module			 : Database Maintenance Plan 
+-- Description		 : run indexes, heaps and statistics maintenance for all databases on the server
+-- ============================================================================
+USE [$(dbName)]
+GO
+DECLARE @databaseName [sysname]
+
+DECLARE crsDatabases CURSOR LOCAL FAST_FORWARD FOR	SELECT [name] 
+							FROM master.dbo.sysdatabases
+							WHERE CASE WHEN [status] & 32 = 32 THEN 'LOADING'
+										WHEN [status] & 64 = 64 THEN 'PRE RECOVERY'
+										WHEN [status] & 128 = 128 THEN 'RECOVERING'
+										WHEN [status] & 256 = 256 THEN 'NOT RECOVERED'
+										WHEN [status] & 512 = 512 THEN 'OFFLINE'
+										WHEN [status] & 2097152 = 2097152 THEN 'STANDBY'
+										WHEN [status] & 1024 = 1024 THEN 'READ ONLY'
+										WHEN [status] & 2048 = 2048 THEN 'DBO USE ONLY'
+										WHEN [status] & 4096 = 4096 THEN 'SINGLE USER'
+										WHEN [status] & 32768 = 32768 THEN 'EMERGENCY MODE'
+										WHEN [status] & 4194584 = 4194584 THEN 'SUSPECT'
+										ELSE 'ONLINE'
+								END = 'ONLINE'
+OPEN crsDatabases
+FETCH NEXT FROM crsDatabases INTO @databaseName
+WHILE @@FETCH_STATUS=0
+	begin
+		EXEC [dbo].[usp_mpDatabaseOptimize]			@SQLServerName			= @@SERVERNAME,
+													@DBName					= @databaseName,
+													@TableSchema			= '%',
+													@TableName				= '%',
+													@flgActions				= 27,
+													@flgOptions				= DEFAULT,
+													@DefragIndexThreshold	= DEFAULT,
+													@RebuildIndexThreshold	= DEFAULT,
+													@DebugMode				= DEFAULT
+				
+		FETCH NEXT FROM crsDatabases INTO @databaseName
+	end
+CLOSE crsDatabases
+DEALLOCATE crsDatabases
+GO
