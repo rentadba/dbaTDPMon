e3eb2586248743dc4d480efeabcf9998b153d48f
Dan Andrei STEFAN <danandrei.stefan@gmail.com>
2015-10-14 23:29:02 +0300

    remove old table

--- source/maintenance-plan/tables/maintenance-plan.statsMaintenancePlanInternals.sql
+++
@@ -1,8 +0,0 @@
------------------------------------------------------------------------------------------------------
---
------------------------------------------------------------------------------------------------------
-RAISERROR('Drop table: [maintenance-plan].[statsMaintenancePlanInternals]', 10, 1) WITH NOWAIT
-GO
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[maintenance-plan].[statsMaintenancePlanInternals]') AND type in (N'U'))
-DROP TABLE [maintenance-plan].[statsMaintenancePlanInternals]
-GO
