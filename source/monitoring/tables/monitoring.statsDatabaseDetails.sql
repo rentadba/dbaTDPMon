3ef0ccf2ebb7997b045dc0e0eb12af089ef92a75
danandreo.stefan <danandrei.stefan@gmail.com>
2017-03-21 23:27:03 +0200

    save health-check database details for later capacity plannig - [monitoring].[vw_statsDatabaseDetails]

---
+++ source/monitoring/tables/monitoring.statsDatabaseDetails.sql
@@ -0,0 +1,57 @@
+-- ============================================================================
+-- Copyright (c) 2004-2015 Dan Andrei STEFAN (danandrei.stefan@gmail.com)
+-- ============================================================================
+-- Author			 : Dan Andrei STEFAN
+-- Create date		 : 21.03.2017
+-- Module			 : Database Analysis & Performance Monitoring
+-- ============================================================================
+
+-----------------------------------------------------------------------------------------------------
+--Health Check: database statistics & details
+-----------------------------------------------------------------------------------------------------
+RAISERROR('Create table: [monitoring].[statsDatabaseDetails]', 10, 1) WITH NOWAIT
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[monitoring].[statsDatabaseDetails]') AND type in (N'U'))
+DROP TABLE [monitoring].[statsDatabaseDetails]
+GO
+CREATE TABLE [monitoring].[statsDatabaseDetails]
+(
+	[id]						[int]	 IDENTITY (1, 1)	NOT NULL,
+	[catalog_database_id]		[smallint]		NOT NULL,
+	[instance_id]				[smallint]		NOT NULL,
+	[recovery_model]			[tinyint]		NULL,
+	[page_verify_option]		[tinyint]		NULL,
+	[compatibility_level]		[tinyint]		NULL,
+	[data_size_mb]				[numeric](20,3)	NULL,
+	[data_space_used_percent]	[numeric](6,2)	NULL,
+	[log_size_mb]				[numeric](20,3)	NULL,
+	[log_space_used_percent]	[numeric](6,2)	NULL,
+	[is_auto_close]				[bit]			NULL,
+	[is_auto_shrink]			[bit]			NULL,
+	[physical_drives]			[sysname]		NULL,
+	[last_backup_time]			[datetime]		NULL,
+	[last_dbcc checkdb_time]	[datetime]		NULL,
+	[is_growth_limited]			[bit]			NULL,
+	[event_date_utc]			[datetime]		NOT NULL,
+	CONSTRAINT [PK_statsDatabaseDetails] PRIMARY KEY  CLUSTERED 
+	(
+		[id],
+		[catalog_database_id]
+	) ON [FG_Statistics_Data],
+	CONSTRAINT [FK_statsDatabaseDetails_catalogDatabaseNames] FOREIGN KEY 
+	(
+		  [catalog_database_id]
+		, [instance_id]
+	) 
+	REFERENCES [dbo].[catalogDatabaseNames] 
+	(
+		  [id]
+		, [instance_id]
+	)
+)ON [FG_Statistics_Data]
+GO
+
+CREATE INDEX [IX_statsDatabaseDetails_CatalogDatabaseID] ON [monitoring].[statsDatabaseDetails]( [catalog_database_id], [instance_id]) ON [FG_Statistics_Index]
+GO
+CREATE INDEX [IX_statsDatabaseDetails_InstanceID] ON [monitoring].[statsDatabaseDetails] ([instance_id]) ON [FG_Statistics_Index]
+GO
