a25c2280ba7d9a8ab9b56fbb64900988a570da64
danandreo.stefan <danandrei.stefan@gmail.com>
2017-05-08 23:51:35 +0300

    fix bugs on maintenance-plan module when running on an AlwaysOn configuration with multiple groups (mix primary/secondary)

--- source/patches/20170508-patch-upgrade-from-v2017_4-to-v2017_5-mp.sql
+++ source/patches/20170508-patch-upgrade-from-v2017_4-to-v2017_5-mp.sql
@@ -1,3 +1,70 @@
+USE [dbaTDPMon]
+GO
+
+RAISERROR('*-----------------------------------------------------------------------------*', 10, 1) WITH NOWAIT
+RAISERROR('* dbaTDPMon (Troubleshoot Database Performance / Monitoring)                  *', 10, 1) WITH NOWAIT
+RAISERROR('* http://dbatdpmon.codeplex.com, under GNU (GPLv3) licence model              *', 10, 1) WITH NOWAIT
+RAISERROR('*-----------------------------------------------------------------------------*', 10, 1) WITH NOWAIT
+RAISERROR('* Patch script: from version 2017.4 to 2017.5 (2017.05.08)				  *', 10, 1) WITH NOWAIT
+RAISERROR('*-----------------------------------------------------------------------------*', 10, 1) WITH NOWAIT
+
+SELECT * FROM [dbo].[appConfigurations] WHERE [module] = 'common' AND [name] = 'Application Version'
+GO
+UPDATE [dbo].[appConfigurations] SET [value] = N'2017.05.08' WHERE [module] = 'common' AND [name] = 'Application Version'
+GO
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+/* patch module: commons																							   */
+/*---------------------------------------------------------------------------------------------------------------------*/
+RAISERROR('Patching module: COMMONS', 10, 1) WITH NOWAIT
+
+
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+/* patch module: maintenance-plan																					   */
+/*---------------------------------------------------------------------------------------------------------------------*/
+RAISERROR('Patching module: MAINTENANCE-PLAN', 10, 1) WITH NOWAIT
+
+RAISERROR('Create function: [dbo].[ufn_mpObjectQuoteName]', 10, 1) WITH NOWAIT
+GO
+IF  EXISTS (SELECT * FROM sysobjects WHERE id = OBJECT_ID(N'[dbo].[ufn_mpObjectQuoteName]') AND xtype in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+DROP FUNCTION [dbo].[ufn_mpObjectQuoteName]
+GO
+
+CREATE FUNCTION [dbo].[ufn_mpObjectQuoteName]
+(		
+	@objectName	[sysname]
+)
+RETURNS [sysname]
+/* WITH ENCRYPTION */
+AS
+
+-- ============================================================================
+-- Copyright (c) 2004-2017 Dan Andrei STEFAN (danandrei.stefan@gmail.com)
+-- ============================================================================
+-- Author			 : Dan Andrei STEFAN
+-- Create date		 : 2004-2017
+-- Module			 : Database Analysis & Performance Monitoring
+-- ============================================================================
+
+begin
+	DECLARE   @quoteCharStart	[varchar](1)
+			, @quoteCharEnd		[varchar](1)
+
+	SET @quoteCharStart	= '['
+	SET @quoteCharEnd	= ']'
+
+	IF CHARINDEX(']', @objectName)<>0 OR CHARINDEX(']', @objectName)<>0
+		begin
+			SET @quoteCharStart	= '"'
+			SET @quoteCharEnd	= '"'								
+		end
+
+	RETURN (@quoteCharStart + @objectName + @quoteCharEnd)
+end
+GO
+
+
 RAISERROR('Create procedure: [dbo].[usp_mpCheckAvailabilityGroupLimitations]', 10, 1) WITH NOWAIT
 GO
 IF  EXISTS (
@@ -457,3 +524,14 @@ IF @agName IS NOT NULL AND @clusterName IS NOT NULL
 
 RETURN 0
 GO
+
+
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+USE [dbaTDPMon]
+GO
+SELECT * FROM [dbo].[appConfigurations] WHERE [module] = 'common' AND [name] = 'Application Version'
+GO
+
+RAISERROR('* Done *', 10, 1) WITH NOWAIT
+
