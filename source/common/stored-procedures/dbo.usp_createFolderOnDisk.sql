eac5a10cecce908bb0aafbdebfef1abd5b0b807b
danandreo.stefan <danandrei.stefan@gmail.com>
2018-01-28 17:06:25 +0200

    print status messages on debugMode=1

--- source/common/stored-procedures/dbo.usp_createFolderOnDisk.sql
+++ source/common/stored-procedures/dbo.usp_createFolderOnDisk.sql
@@ -63,9 +63,6 @@ SET @folderName = [dbo].[ufn_getObjectQuoteName](@folderName, 'filepath')
 
 SET @folderName = [dbo].[ufn_formatPlatformSpecificPath](@sqlServerName, @folderName)
 
-SET @queryToRun= 'Creating destination folder: "' + @folderName + '"'
-EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 0, @stopExecution=0
-
 -----------------------------------------------------------------------------------------
 --get destination server running version/edition
 SET @nestedExecutionLevel = @executionLevel + 1
@@ -103,102 +100,111 @@ SET @retryAttempts=3
 
 IF (SELECT [file_is_directory] FROM #fileExists)=1
 	begin
-		SET @queryToRun = N'Folder already exists.'
-		EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
+		IF @debugMode=1	
+			begin
+				SET @queryToRun = N'Folder already exists.'
+				EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
+			end
+		RETURN
 	end
 ELSE
-	WHILE (SELECT [file_is_directory] FROM #fileExists)=0 AND @retryAttempts > 0
-		begin
-			SET @runWithxpCreateSubdir = 0
-			SET @optionXPValue = 0
+	begin
+		SET @queryToRun= 'Creating destination folder: "' + @folderName + '"'
+		EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 0, @stopExecution=0
 
-			IF @serverVersionNum>=9
-				begin
-					/*-------------------------------------------------------------------------------------------------------------------------------*/
-					SET @queryToRun = N'[' + @sqlServerName + '].master.sys.xp_create_subdir N''' + @folderName + ''''
-					IF @debugMode=1	EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
-					EXEC (@queryToRun)
+		WHILE (SELECT [file_is_directory] FROM #fileExists)=0 AND @retryAttempts > 0
+			begin
+				SET @runWithxpCreateSubdir = 0
+				SET @optionXPValue = 0
+
+				IF @serverVersionNum>=9
+					begin
+						/*-------------------------------------------------------------------------------------------------------------------------------*/
+						SET @queryToRun = N'[' + @sqlServerName + '].master.sys.xp_create_subdir N''' + @folderName + ''''
+						IF @debugMode=1	EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
+						EXEC (@queryToRun)
 				
-					IF @@ERROR=0
-						SET @runWithxpCreateSubdir=1
-					ELSE
-						begin
-							/* enable xp_cmdshell configuration option */
-							EXEC [dbo].[usp_changeServerOption_xp_cmdshell]   @serverToRun	 = @sqlServerName
-																			, @flgAction	 = 1			-- 1=enable | 0=disable
-																			, @optionXPValue = @optionXPValue OUTPUT
-																			, @debugMode	 = @debugMode
+						IF @@ERROR=0
+							SET @runWithxpCreateSubdir=1
+						ELSE
+							begin
+								/* enable xp_cmdshell configuration option */
+								EXEC [dbo].[usp_changeServerOption_xp_cmdshell]   @serverToRun	 = @sqlServerName
+																				, @flgAction	 = 1			-- 1=enable | 0=disable
+																				, @optionXPValue = @optionXPValue OUTPUT
+																				, @debugMode	 = @debugMode
 
-							IF @optionXPValue = 0
-								begin
-									RETURN 1
-								end		
-						end
-				end
+								IF @optionXPValue = 0
+									begin
+										RETURN 1
+									end		
+							end
+					end
 
-			/*-------------------------------------------------------------------------------------------------------------------------------*/
-			/* creating folder   																											 */
-			IF @runWithxpCreateSubdir=0
-				begin
-					SET @queryToRun = N'MKDIR -P "' + @folderName + '"'
-					SET @serverToRun = N'[' + @sqlServerName + '].master.dbo.xp_cmdshell'
-					IF @debugMode=1	EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
-					EXEC @serverToRun @queryToRun , NO_OUTPUT
-				end
+				/*-------------------------------------------------------------------------------------------------------------------------------*/
+				/* creating folder   																											 */
+				IF @runWithxpCreateSubdir=0
+					begin
+						SET @queryToRun = N'MKDIR -P "' + @folderName + '"'
+						SET @serverToRun = N'[' + @sqlServerName + '].master.dbo.xp_cmdshell'
+						IF @debugMode=1	EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
+						EXEC @serverToRun @queryToRun , NO_OUTPUT
+					end
 
-			/*-------------------------------------------------------------------------------------------------------------------------------*/
-			IF @serverVersionNum>=9 AND @runWithxpCreateSubdir=0
-				begin
-					/* disable xp_cmdshell configuration option */
-					EXEC [dbo].[usp_changeServerOption_xp_cmdshell]   @serverToRun	 = @sqlServerName
-																	, @flgAction	 = 0			-- 1=enable | 0=disable
-																	, @optionXPValue = @optionXPValue OUTPUT
-																	, @debugMode	 = @debugMode
+				/*-------------------------------------------------------------------------------------------------------------------------------*/
+				IF @serverVersionNum>=9 AND @runWithxpCreateSubdir=0
+					begin
+						/* disable xp_cmdshell configuration option */
+						EXEC [dbo].[usp_changeServerOption_xp_cmdshell]   @serverToRun	 = @sqlServerName
+																		, @flgAction	 = 0			-- 1=enable | 0=disable
+																		, @optionXPValue = @optionXPValue OUTPUT
+																		, @debugMode	 = @debugMode
 
-				end
-
-
-			---------------------------------------------------------------------------------------------
-			/* get configuration values - wait/lock timeout */
-			DECLARE @queryLockTimeOut [int]
-			SELECT	@queryLockTimeOut=[value] 
-			FROM	[dbo].[appConfigurations] 
-			WHERE	[name] = 'Default lock timeout (ms)'
-					AND [module] = 'common'
+					end
 
 
-			SET @queryLockTimeOut = @queryLockTimeOut / 1000
-			DECLARE @waitDelay [varchar](16)
+				---------------------------------------------------------------------------------------------
+				/* get configuration values - wait/lock timeout */
+				DECLARE @queryLockTimeOut [int]
+				SELECT	@queryLockTimeOut=[value] 
+				FROM	[dbo].[appConfigurations] 
+				WHERE	[name] = 'Default lock timeout (ms)'
+						AND [module] = 'common'
 
-			SET @waitDelay = REPLICATE('0', 2-LEN(CAST(@queryLockTimeOut/3600 AS [varchar]))) + CAST(@queryLockTimeOut/3600 AS [varchar]) + ':' + 
-							 REPLICATE('0', 2-LEN(CAST((@queryLockTimeOut%3600)/60 AS [varchar]))) + CAST((@queryLockTimeOut%3600)/60 AS [varchar]) + ':' +
-							 REPLICATE('0', 2-LEN(CAST(@queryLockTimeOut%60 AS [varchar]))) + CAST(@queryLockTimeOut%60 AS [varchar])
 
-			--wait 5 seconds before
-			WAITFOR DELAY @waitDelay
+				SET @queryLockTimeOut = @queryLockTimeOut / 1000
+				DECLARE @waitDelay [varchar](16)
 
-			/*-------------------------------------------------------------------------------------------------------------------------------*/
-			/* check if folderName exists																									 */
-			IF @sqlServerName=@@SERVERNAME
-					SET @queryToRun = N'master.dbo.xp_fileexist ''' + @folderName + ''''
-			else
-				begin
-					IF @serverVersionNum < 11	
-						SET @queryToRun = N'SELECT * FROM OPENQUERY([' + @sqlServerName + N'], ''SET FMTONLY OFF; EXEC master.dbo.xp_fileexist ''''' + @folderName + ''''';'')x'
-					ELSE
-						SET @queryToRun = N'SELECT * FROM OPENQUERY([' + @sqlServerName + N'], ''SET FMTONLY OFF; EXEC(''''master.dbo.xp_fileexist ''''''''' + @folderName + ''''''''' '''') WITH RESULT SETS(([File Exists] [int], [File is a Directory] [int], [Parent Directory Exists] [int])) '')x'
-				end
+				SET @waitDelay = REPLICATE('0', 2-LEN(CAST(@queryLockTimeOut/3600 AS [varchar]))) + CAST(@queryLockTimeOut/3600 AS [varchar]) + ':' + 
+								 REPLICATE('0', 2-LEN(CAST((@queryLockTimeOut%3600)/60 AS [varchar]))) + CAST((@queryLockTimeOut%3600)/60 AS [varchar]) + ':' +
+								 REPLICATE('0', 2-LEN(CAST(@queryLockTimeOut%60 AS [varchar]))) + CAST(@queryLockTimeOut%60 AS [varchar])
 
-			IF @debugMode=1	EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
-			DELETE FROM #fileExists
-			INSERT	INTO #fileExists([file_exists], [file_is_directory], [parent_directory_exists])
-					EXEC (@queryToRun)
+				--wait 5 seconds before
+				WAITFOR DELAY @waitDelay
 
-			IF (SELECT [file_is_directory] FROM #fileExists)=0
-				SET @retryAttempts=@retryAttempts - 1
-			ELSE
-				SET @retryAttempts=0
-		end
+				/*-------------------------------------------------------------------------------------------------------------------------------*/
+				/* check if folderName exists																									 */
+				IF @sqlServerName=@@SERVERNAME
+						SET @queryToRun = N'master.dbo.xp_fileexist ''' + @folderName + ''''
+				else
+					begin
+						IF @serverVersionNum < 11	
+							SET @queryToRun = N'SELECT * FROM OPENQUERY([' + @sqlServerName + N'], ''SET FMTONLY OFF; EXEC master.dbo.xp_fileexist ''''' + @folderName + ''''';'')x'
+						ELSE
+							SET @queryToRun = N'SELECT * FROM OPENQUERY([' + @sqlServerName + N'], ''SET FMTONLY OFF; EXEC(''''master.dbo.xp_fileexist ''''''''' + @folderName + ''''''''' '''') WITH RESULT SETS(([File Exists] [int], [File is a Directory] [int], [Parent Directory Exists] [int])) '')x'
+					end
+
+				IF @debugMode=1	EXEC [dbo].[usp_logPrintMessage] @customMessage = @queryToRun, @raiseErrorAsPrint = 0, @messagRootLevel = @executionLevel, @messageTreelevel = 1, @stopExecution=0
+				DELETE FROM #fileExists
+				INSERT	INTO #fileExists([file_exists], [file_is_directory], [parent_directory_exists])
+						EXEC (@queryToRun)
+
+				IF (SELECT [file_is_directory] FROM #fileExists)=0
+					SET @retryAttempts=@retryAttempts - 1
+				ELSE
+					SET @retryAttempts=0
+			end
+	end
 
 IF (SELECT [file_is_directory] FROM #fileExists)=0
 	begin
