c60c5ccb6d7c916a78b0e500242d2e71e0f494ae
Dan Andrei STEFAN <dan.andreistefan@aurea.com>
2018-12-27 11:04:05 +0200

    fix defect with internal job scheduler and update statistics task

--- source/setup/update.bat
+++ source/setup/update.bat
@@ -578,7 +578,7 @@ if errorlevel 1 goto install_err
 
 
 :done
-if "%run2kmode%"=="false" sqlcmd.exe -S%server% %autentif% -Q "SET NOCOUNT ON; UPDATE [dbo].[appConfigurations] SET [value] = N'2018.12.26' WHERE [module] = 'common' AND [name] = 'Application Version'" -d %dbname%  -b -r 1
+if "%run2kmode%"=="false" sqlcmd.exe -S%server% %autentif% -Q "SET NOCOUNT ON; UPDATE [dbo].[appConfigurations] SET [value] = N'2018.12.27' WHERE [module] = 'common' AND [name] = 'Application Version'" -d %dbname%  -b -r 1
 if errorlevel 1 goto install_err  
 
 echo *-----------------------------------------------------------------------------*
