8347c6eb638c9f8af8d3b994bb202e19125f334a
Dan Andrei STEFAN <dan.andreistefan@aurea.com>
2019-11-29 21:39:21 +0200

    allow only update from v2019.7 on-wards

--- source/setup/detect-version.sql
+++ source/setup/detect-version.sql
@@ -7,4 +7,7 @@ SET NOCOUNT ON
 DECLARE @appVersion [sysname]
 SELECT @appVersion = [value] FROM [dbo].[appConfigurations] WHERE [module] = 'common' AND [name] = 'Application Version'
 PRINT 'Detected dbaTDPMon version: ' + @appVersion
+
+IF CONVERT([datetime], @appVersion, 102) < '2019-07-12'
+	RAISERROR('ERROR: You must upgrade to dbaTDPMon version 2019.7 before upgrading to last version.', 16, 1) WITH NOWAIT
 GO
